6f4c54f0f0ea070d053e348cc98192574b070647
Honore <himbazaalain022@gmail.com>
2023-09-23 21:49:37 +0200

    translating functionalitites

---
+++ ApiTranslateASP.NETWEB/Controllers/SavedTranslationsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ApiTranslateASP.NETWEB.Models;
+using System.Collections.Generic;
+using ApiTranslateASP.NETWEB.Contracts;
+
+
+namespace ApiTranslateASP.NETWEB.Controllers
+{
+    public class SavedTranslationsController : Controller
+    {
+        private readonly ITranslationRepository _translationRepository;
+
+        public SavedTranslationsController(ITranslationRepository translationRepository)
+        {
+            _translationRepository = translationRepository;
+        }
+
+        public IActionResult SavedTranslations()
+        {
+            IEnumerable<Translation> savedTranslations = _translationRepository.GetAllTranslations();
+            return View("~/Views/Translate/SavedTranslations.cshtml");
+        }
+    }
+}
---
+++ ApiTranslateASP.NETWEB/Controllers/TranslateController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiTranslateASP.NETWEB.Controllers
+{
+    public class TranslateController : Controller
+    {
+        private const string BaseUrl = "https://api.funtranslations.com/translate/leetspeak.json";
+        private readonly HttpClient _httpClient;
+
+        public TranslateController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        public IActionResult Translate()
+        {
+            return View("~/Views/Translate/Index.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Translate(string inputText)
+        {
+            if (string.IsNullOrWhiteSpace(inputText))
+            {
+                ViewBag.TranslatedText = "Input text is empty or contains only whitespace.";
+            }
+            else
+            {
+                try
+                {
+                    // Translate input text to Leetspeak using the API
+                    var translationResult = await TranslateToLeetspeakAsync(inputText);
+
+                    // Console the original input text and translated text
+                    Console.WriteLine("Input Text: " + inputText);
+                    Console.WriteLine("Leetspeak Text: " + translationResult.Contents.Translated);
+
+                    ViewBag.TranslatedText = translationResult.Contents.Translated;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Translation Error: " + ex.Message);
+                    ViewBag.TranslatedText = "Translation Error";
+                }
+            }
+
+            return View("~/Views/Translate/Index.cshtml");
+        }
+
+        private async Task<TranslationResponse> TranslateToLeetspeakAsync(string inputText)
+        {
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("text", inputText)
+            });
+
+            var response = await _httpClient.PostAsync(BaseUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                return Newtonsoft.Json.JsonConvert.DeserializeObject<TranslationResponse>(responseContent);
+            }
+            else
+            {
+                Console.WriteLine($"Translation API error: {response.StatusCode}");
+                throw new Exception($"Translation API error: {response.StatusCode}");
+            }
+        }
+    }
+}
+
+public class TranslationResponse
+{
+    public TranslationContents Contents { get; set; }
+}
+
+public class TranslationContents
+{
+    public string Translated { get; set; }
+}
