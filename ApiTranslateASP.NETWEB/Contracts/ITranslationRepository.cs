dcdc2e0c7ff19a473addf8d5b28e4cd8dfcc58a9
Honore <himbazaalain022@gmail.com>
2023-09-23 21:49:37 +0200

    translating functionalitites

---
+++ ApiTranslateASP.NETWEB/Contracts/ITranslationRepository.cs
@@ -0,0 +1,13 @@
+﻿
+using global::ApiTranslateASP.NETWEB.Models;
+using System.Collections.Generic;
+using ApiTranslateASP.NETWEB.Contracts;
+
+namespace ApiTranslateASP.NETWEB.Contracts
+{
+    public interface ITranslationRepository
+    {
+        IEnumerable<Translation> GetAllTranslations();
+        void AddTranslation(Translation translation);
+    }
+}
---
+++ ApiTranslateASP.NETWEB/Models/Translation.cs.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiTranslateASP.NETWEB.Models
+{
+    public class Translation
+    {
+        public int Id { get; set; }
+        public string OriginalText { get; set; }
+        public string TranslatedText { get; set; }
+    }
+}
