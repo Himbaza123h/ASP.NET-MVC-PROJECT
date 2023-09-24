42bd9d756cfbb15c163dc36c2dd58278983607be
Honore <himbazaalain022@gmail.com>
2023-09-25 01:14:51 +0200

    AJAX helpers to load the data

--- ApiTranslateASP.NETWEB/Models/Translation.cs.cs
+++ ApiTranslateASP.NETWEB/Models/Translation.cs.cs
@@ -6,7 +6,7 @@ namespace ApiTranslateASP.NETWEB.Models
     public class Translation
     {
         [BsonId]
-        
+        public int Id { get; set; }  
         public string OriginalText { get; set; }
         public string TranslatedText { get; set; }
     }
