adc71ffa06c16354d4ee0264c33ba51657d8bacd
Honore <himbazaalain022@gmail.com>
2023-09-25 13:37:33 +0200

    Model modification based on controller

--- ApiTranslateASP.NETWEB/Models/Translation.cs.cs
+++ ApiTranslateASP.NETWEB/Models/Translation.cs.cs
@@ -6,7 +6,7 @@ namespace ApiTranslateASP.NETWEB.Models
     public class Translation
     {
         [BsonId]
-        public int Id { get; set; }  
+        public ObjectId Id { get; set; }  
         public string OriginalText { get; set; }
         public string TranslatedText { get; set; }
     }
